35adfc7f4fad60226337fa6e4ad5a37a4bdba7ee
fp0 <davidbjbelanger@gmail.com>
2015-01-19 09:30:14 -0800

    initial commit

---
+++ CKEditor.js
@@ -0,0 +1,61 @@
+﻿/*
+ExtJS 4.x wrapper for CKEditor (http://ckeditor.com/)
+*/
+
+Ext.define('Ext.ux.CKEditor', {
+    extend: 'Ext.form.field.TextArea',
+    alias: 'widget.ckeditor',
+
+    constructor: function () {
+        this.callParent(arguments);
+        this.addEvents("instanceReady");
+    },
+
+    initComponent: function () {
+        this.callParent(arguments);
+        this.on("afterrender", function () {
+            Ext.apply(this.CKConfig, {
+                height: this.getHeight(),
+                width: this.getWidth()
+            });
+
+            this.editor = CKEDITOR.replace(this.inputEl.id, this.CKConfig);
+            this.editor.name = this.name;
+
+            this.editor.on("instanceReady", function () {
+                this.fireEvent(
+                    "instanceReady", 
+                    this, 
+                    this.editor
+                );
+            }, this);
+
+        }, this);
+    },
+
+    onRender: function (ct, position) {
+        if (!this.el) {
+            this.defaultAutoCreate = {
+                tag: 'textarea',
+                autocomplete: 'off'
+            };
+        }
+        this.callParent(arguments)
+    },
+    
+    setValue: function (value) {
+        this.callParent(arguments);
+        if (this.editor) {
+            this.editor.setData(value);
+        }
+    },
+
+    getValue: function () {
+        if (this.editor) {
+            return this.editor.getData();
+        } 
+        else {
+            return ''
+        }
+    }
+});
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# extjs4-ckeditor
-Use CKEditor with ExtJS 4.x
+# ExtJS 4.x + CKEditor
+
+Here is a simple page using a wrapper for the [CKEditor](http://ckeditor.com/) to be used in ExtJS.
+
+## How to use (Non-MVC)
+
+See the sample index.html file included.
+
+## How to use (MVC)
+
+1. Include the CKEditor either with a local downloaded copy or through the CKEditor CDN.
+
+2. Copy the js file `'CKEditor.js'` to your project
+
+3. Change the class name if desired `'Ext.ux.CKEditor'`
+
+4. Add requires:
+
+	```
+	 requires: [
+		'Ext.ux.CKEditor'
+	  ]
+	```
+
+5. Use the editor:
+
+	```
+	Ext.create('Ext.panel.Panel', {
+		width: 600,
+		height: 400,
+		...
+		items: [
+			{
+				xtype: 'ckeditor',
+				itemId: 'textEdit',
+				width: 600
+			}
+		]
+	});
+	```
+	
+## Testing
+
+Tested with ExtJS 4.2.1, 4.2.3
+
+## License
+
+MIT License. Free to use and change.
